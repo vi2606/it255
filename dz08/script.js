ed309434dc123c7a6978e1a0052a7cddefcb92e4
Vladimir Ilic <vladimir.ilic.2606@metropolitan.ac.rs>
2018-09-16 09:17:04 +0200

    Old style

---
+++ dz08/script.js
@@ -0,0 +1,25 @@
+$('.navbar-nav>li>a').on('click', function () {
+    $('.navbar-collapse').collapse('hide');
+});
+
+
+
+function modalTurnOn() {
+    document.getElementById('id01').style.display = 'block';
+
+    // had to remove fixed top b/c it didnt fade
+    // had to add body class b/c of padding for fixed nav
+    $('body').toggleClass("body");
+    $('#main-nav').removeClass("fixed-top");
+
+}
+
+function modalTurnOff() {
+    document.getElementById('id01').style.display = 'none';
+
+    // $('body').css("padding-top: 1px;");
+    $('body').toggleClass("body");
+    // $('#main-nav').removeClass("fixed-top");
+    $('#main-nav').addClass("fixed-top");
+
+}
